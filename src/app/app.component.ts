cc84947392c4b5b9decb598bca90ffd08f9077c3
omar264 <gharianiomar10@gmail.com>
2021-02-26 20:51:34 +0100

    saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'application-form-frontend';
+  public title = 'application-form-frontend';
 }
