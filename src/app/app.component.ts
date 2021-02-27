0ea272e3950657e5f0851e6d306096282dce9374
omar264 <gharianiomar10@gmail.com>
2021-02-27 21:02:39 +0100

    saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthenticationService } from './_services';
+import { User } from './_models';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
   public title = 'application-form-frontend';
+
+    currentUser: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
 }
