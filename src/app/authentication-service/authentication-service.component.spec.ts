fa40439ee1e78a1b189ebb2c9cd13e0d81bf65f5
omar264 <gharianiomar10@gmail.com>
2021-02-28 02:24:37 +0100

    saving

---
+++ src/app/authentication-service/authentication-service.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthenticationServiceComponent } from './authentication-service.component';
+
+describe('AuthenticationServiceComponent', () => {
+  let component: AuthenticationServiceComponent;
+  let fixture: ComponentFixture<AuthenticationServiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AuthenticationServiceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuthenticationServiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/authentication-service/authentication-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication-service',
+  templateUrl: './authentication-service.component.html',
+  styleUrls: ['./authentication-service.component.css']
+})
+export class AuthenticationServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
