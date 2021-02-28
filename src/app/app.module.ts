a8bf3442086b6c5dd5d0291f0d5d3b65e9178552
omar264 <gharianiomar10@gmail.com>
2021-02-28 03:35:45 +0100

    saving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthenticationServiceComponent } from './authentication-service/authentication-service.component';
 import { MyserviceService } from './myservice.service';
-/*import { HttpClientModule } from '@angular/common/http'*/
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
