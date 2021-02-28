71f3c7e8d0fe8010c4b00652e7cd03a014081266
omar264 <gharianiomar10@gmail.com>
2021-02-28 04:07:21 +0100

    saving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { AuthenticationServiceComponent } from './authentication-service/authentication-service.component';
 import { MyserviceService } from './myservice.service';
 import { HttpClientModule } from '@angular/common/http';
+import { RecruiterComponent } from './recruiter/recruiter.component';
+import { JobSeekerComponent } from './job-seeker/job-seeker.component';
 @NgModule({
   declarations: [
     AppComponent,
     AuthenticationService,
-    AuthenticationServiceComponent
+    AuthenticationServiceComponent,
+    RecruiterComponent,
+    JobSeekerComponent
 
   ],
   imports: [
