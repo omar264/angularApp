c7279a415fa9832e80b89b8bb018d8ce94dd6382
omar264 <gharianiomar10@gmail.com>
2021-02-28 03:16:26 +0100

    saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from './_services';
 import { User } from './_models';
+import { Component } from '@angular/core';
+import { MyserviceService } from './myservice.myservice';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,16 +12,25 @@ import { User } from './_models';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
+todaydate;
+
   public title = 'application-form-frontend';
 
     currentUser: User;
 
     constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) {
-        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private myservice: MyserviceService  ){}
+
+    ngOnInit(){
+    this.todaydate = this.myservice.showTodayDate();
     }
+
+
+
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+
 
     logout() {
         this.authenticationService.logout();
